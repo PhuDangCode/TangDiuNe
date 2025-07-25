5714688937649abf14fdcc78b6f69401921bd15c
PhuDangCode <10421047@student.vgu.edu.vn>
2025-07-26 05:00:24 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Sumi';
+const giftUrl = '';
+const eventName = 'Tặng nè';
+const titleCard = 'Mở ra đi';
+const contentCard = 'cảm ơn nhiều vì đã ở bên lúc mệt mỏi và buồn nhất, chúc mọi thứ sắp tới luôn thành công nhe!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'mi.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
